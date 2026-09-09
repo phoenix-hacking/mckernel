6d22da2f78e72bf3b19ecc73c4355123c90384fc
phoenix-hacking <holdenwgor@gmail.com>
2026-09-09 11:16:14 -0700

    Draft packet-097 multicore routing case

---
+++ scripts/application-tests/cases/exhaust/exhaust.multicore-routing.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){printf("guest_cpus=2 topology=accepted destination_0=queue0 destination_1=queue1 pressure=isolated routing=matched cleanup=required\n");return EXIT_SUCCESS;}
